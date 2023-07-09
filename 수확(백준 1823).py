02fbd2674e51d1ffaa68bb67a249e91ad00b7dea
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-07-09 22:42:10 +0900

    Update 수확(백준 1823).py

--- 수확(백준 1823).py
+++ 수확(백준 1823).py
@@ -1,13 +1,5 @@
 import sys
 
-input = sys.stdin.readline
-sys.setrecursionlimit(2000)
-
-n = int(input())
-rice = [int(input()) for _ in range(n)]
-
-dp = [[0 for _ in range(n)] for _ in range(n)]
-
 
 def get_maxvalue(s, e, cnt):
     # base case
@@ -23,4 +15,15 @@ def get_maxvalue(s, e, cnt):
     return dp[s][e]
 
 
+input = sys.stdin.readline
+sys.setrecursionlimit(2000)
+
+n = int(input())
+rice = [int(input()) for _ in range(n)]
+
+dp = [[0 for _ in range(n)] for _ in range(n)]
+
+
 print(get_maxvalue(0, n - 1, 1))
+# top-down 접근을 통해 Left에서 시작해 Right까지 최대 합 + 선택된 숫자 결과를 가져온다.
+# 이러한 중간 결과들을 모두 dp에 저장하면된다.
