a62120548d0dfca3187701cd2415f56f7da858a3
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-15 11:36:38 +0900

    재풀이

--- 진우의 달 여행(Large).py
+++ 진우의 달 여행(Large).py
@@ -1,24 +1,30 @@
-N,M = map(int, input().split(" "))
+N, M = map(int, input().split(" "))
 board = [list(map(int, input().split(" "))) for _ in range(N)]
-INF = float('inf')
-dp = [[[INF]*3 for _ in range(M)] for _ in range(N)]
+dp = [[[1e9] * 3 for _ in range(M)] for _ in range(N)]
 
-for j in range(M):    
-    dp[0][j][0] = board[0][j]
-    dp[0][j][1] = board[0][j]
-    dp[0][j][2] = board[0][j]
+dp[0] = [[board[0][i], board[0][i], board[0][i]] for i in range(M)]
 
-for i in range(1,N):
+moves = [[-1, -1], [-1, 0], [-1, 1]]
+for i in range(1, N):
     for j in range(M):
-        if j-1 >= 0:
-            dp[i][j][0] = min(dp[i-1][j-1][2], dp[i-1][j-1][1]) + board[i][j]        
-        if j+1 <= M-1:
-            dp[i][j][2] = min(dp[i-1][j+1][1], dp[i-1][j+1][0]) + board[i][j]
-        dp[i][j][1] = min(dp[i-1][j][2], dp[i-1][j][0]) + board[i][j]
+        for k in range(3):
+            dx, dy = moves[k]
+            if 0 <= i + dx < N and 0 <= j + dy < M:
+                if k == 0:
+                    dp[i][j][k] = (
+                        min(dp[i + dx][j + dy][1], dp[i + dx][j + dy][2]) + board[i][j]
+                    )
+                elif k == 1:
+                    dp[i][j][k] = (
+                        min(dp[i + dx][j + dy][0], dp[i + dx][j + dy][2]) + board[i][j]
+                    )
+                else:
+                    dp[i][j][k] = (
+                        min(dp[i + dx][j + dy][0], dp[i + dx][j + dy][1]) + board[i][j]
+                    )
 
-answer = INF
+result = 1e9
 for i in range(M):
-    answer = min(min(dp[N-1][i]), answer)
+    result = min(min(dp[N - 1][i]), result)
 
-print(answer)
-#3D 배열을 만들 때 for문 2개로 사용해야한다 마지막만 []*숫자 구문 사용
+print(result)
--- 함께 블록 쌓기(백준 18427).py
+++ 함께 블록 쌓기(백준 18427).py
@@ -1,21 +1,11 @@
-N, M, H = map(int, input().split(" "))#N 인원 수, M 인당 최대 블록, H 만들고자하는 탑 높이
+N, M, H = map(int, input().split(" "))
+dp = [[0] * 1001 for _ in range(N + 1)]
 blocks = [list(map(int, input().split(" "))) for _ in range(N)]
-dp = [[0]*(H+1)for _ in range(N)]
 dp[0][0] = 1
-for block in blocks[0]:
-    dp[0][block] = 1
+for i in range(1, N + 1):
+    dp[i] = dp[i - 1][0:]
+    for b in blocks[i - 1]:
+        for j in range(b, H + 1):
+            dp[i][j] += dp[i - 1][j - b]
 
-for i in range(1,N):
-    for b in range((len(blocks[i])+1)):
-        block = 0
-        if b > 0:
-            block = blocks[i][b-1]
-            for j in range((H+1)):
-                if dp[i-1][j] >= 1:
-                    if block + j <= H:
-                        dp[i][block + j] += dp[i-1][j]
-        else:
-            for j in range((H+1)):                                    
-                dp[i][block + j] = dp[i-1][block+j]
-
-print(dp[N-1][-1]%10_007)
+print(dp[N][H] % 10007)
