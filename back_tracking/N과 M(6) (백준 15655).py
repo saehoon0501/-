dffaca65dff62ba4f01bbb5f00698107098e7fee
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-09-23 03:58:31 +0900

    백트래킹 연습

---
+++ back_tracking/N과 M(6) (백준 15655).py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+sequence = list(map(int, input().split()))
+sequence.sort()
+
+
+def printSequence(numbers, count, start):
+    if count == M:
+        print(*numbers)
+        return
+
+    for i in range(start, N):
+        numbers.append(sequence[i])
+        printSequence(numbers, count + 1, i + 1)
+        numbers.pop()
+    return
+
+
+printSequence([], 0, 0)
---
+++ back_tracking/N과 M(9) (백준 15658)..py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+sequence = list(map(int, input().split()))
+sequence.sort()
+index = []
+
+
+def printSequence(numbers, count):
+    if count == M:
+        print(*numbers)
+        return
+    PREV = 0
+    for i in range(N):
+        if i not in index and sequence[i] != PREV:
+            numbers.append(sequence[i])
+            index.append(i)
+            PREV = sequence[i]
+            printSequence(numbers, count + 1)
+            numbers.pop()
+            index.pop()
+
+    return
+
+
+printSequence([], 0)
