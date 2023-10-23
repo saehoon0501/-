08b0c82c7c39d7d24422f5bb2f956d5d6d083988
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-10-23 19:42:06 +0900

    Update 연속합(백준 1912).py

--- DP/연속합(백준 1912).py
+++ DP/연속합(백준 1912).py
@@ -1,12 +1,9 @@
-N = int(input())
+n = int(input())
 numbers = list(map(int, input().split()))
-dp = [0] * N
-
+dp = [0] * n
 dp[0] = numbers[0]
-for i in range(1, N):
-    if numbers[i] < dp[i - 1] + numbers[i]:
-        dp[i] = dp[i - 1] + numbers[i]
-    else:
-        dp[i] = numbers[i]
+
+for i in range(1, n):
+    dp[i] = max(dp[i - 1] + numbers[i], numbers[i])
 
 print(max(dp))
