55f789fa6418431062b6cedb5eb07c546476f26c
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-10-23 22:50:23 +0900

    Update 가장 긴 짝수 연속한 부분 수열(백준 22857).py

--- DP/가장 긴 짝수 연속한 부분 수열(백준 22857).py
+++ DP/가장 긴 짝수 연속한 부분 수열(백준 22857).py
@@ -1,27 +1,22 @@
 N, K = map(int, input().split())
 numbers = list(map(int, input().split()))
-dp = [[0] * 2 for _ in range(N)]
+dp = [[0] * (K + 1) for _ in range(N)]
 
-for i in range(N):
+if numbers[0] % 2 == 0:
+    dp[0][0] = 1
+
+
+for i in range(1, N):
     if numbers[i] % 2 == 0:
-        dp[i] = [1, K]
-
-        count = 0
-        for j in range(i - 1, -1, -1):
-            if K < count:
-                break
-            if numbers[j] % 2 == 0:
-                if count <= dp[j][1]:
-                    dp[i][0] += dp[j][0]
-                    dp[i][1] = dp[j][1] - count
-                    break
-                else:
-                    dp[i][0] += 1
-                    dp[i][1] = K - count
-            else:
-                count += 1
+        dp[i][0] = 1
+        for j in range(min(i, K + 1)):
+            dp[i][j] = dp[i - 1][j] + 1
+    else:
+        for j in range(min(i, K + 1)):
+            if j + 1 < K + 1:
+                dp[i][j + 1] = dp[i - 1][j]
 
 result = 0
-for val, count in dp:
-    result = max(result, val)
+for r in dp:
+    result = max(result, max(r))
 print(result)
